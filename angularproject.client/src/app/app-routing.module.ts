d1a4bfe14c6db11ad3a4177f220be251aec3b22f
AliAlsmadi993 <alsmadiali993@gmail.com>
2025-03-27 15:56:20 +0300

    ali2

--- angularproject.client/src/app/app-routing.module.ts
+++ angularproject.client/src/app/app-routing.module.ts
@@ -1,53 +1,63 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Components from 'adnan'
 import { LoginComponent } from './adnan/login/login.component';
 import { RegisterComponent } from './adnan/register/register.component';
 import { AboutComponent } from './adnan/about/about.component';
 import { ContactComponent } from './adnan/contact/contact.component';
 import { HomeComponent } from './adnan/home/home.component';
+
+// Components from 'ali'
 import { CartComponent } from './ali/cart/cart.component';
 import { CheckoutComponent } from './ali/checkout/checkout.component';
 import { VoucherComponent } from './ali/voucher/voucher.component';
 
+// Components from 'mona'
 import { UsersComponent } from './mona/users/users.component';
 import { VouchersComponent } from './mona/vouchers/vouchers.component';
 import { FeedbackComponent } from './mona/feedback/feedback.component';
 import { DashboardComponent } from './mona/dashboard/dashboard.component';
+
+// Components from 'amal'
+import { ProfileandeditComponent } from './amal/profileandedit/profileandedit.component';
+import { HistoryComponent } from './amal/history/history.component';
+
+// Components from 'Toqa' (Ensure all paths are unified)
 import { AddCategoryComponent } from './Toqa/add-category/add-category.component';
-import { ShowAllCategoryComponent } from './Toqa/show-all-category/show-all-category.component';
 import { EditCategoryComponent } from './Toqa/edit-category/edit-category.component';
 import { AddProductComponent } from './Toqa/add-product/add-product.component';
-import { ProfileandeditComponent} from './Amal/profileandedit/profileandedit.component';
-import { HistoryComponent } from './Amal/history/history.component';
-
+import { ShowAllCategoryComponent } from './Toqa/show-all-category/show-all-category.component';
 
 const routes: Routes = [
-  { path: "users", component: UsersComponent },
-  { path: "vouchers", component: VouchersComponent },
-  { path: "feedback", component: FeedbackComponent },
-  { path: "dashboard", component: DashboardComponent },
+  // 'adnan' routes
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
 
-  { path: "AddCategory", component: AddCategoryComponent },
-  { path: "AllCategories", component: ShowAllCategoryComponent },
-  { path: "EditCategory/:id", component: EditCategoryComponent },
-  { path: "AddProduct", component: AddProductComponent },
+  // 'ali' routes
+  { path: 'carts', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'Voucher', component: VoucherComponent },
 
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "about", component: AboutComponent },
-  { path: "contact", component: ContactComponent },
-  { path: "", pathMatch: "full", component: HomeComponent },
-  { path: "home", component: HomeComponent },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "profile", component: ProfileandeditComponent },
-  { path: "history", component: HistoryComponent }
-];
+  // 'mona' routes
+  { path: 'users', component: UsersComponent },
+  { path: 'vouchers', component: VouchersComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: 'dashboard', component: DashboardComponent },
 
+  // 'amal' routes
+  { path: 'profile', component: ProfileandeditComponent },
+  { path: 'history', component: HistoryComponent },
 
-const routes: Routes = [
-  { path: "carts", component: CartComponent },
-  { path: "checkout", component: CheckoutComponent },
-  { path: "Voucher", component:VoucherComponent }
+  // 'Toqa' routes (Ensure casing consistency)
+  { path: 'AllCategories', component: ShowAllCategoryComponent },
+  { path: 'EditCategory/:id', component: EditCategoryComponent },
+  { path: 'AddProduct', component: AddProductComponent },
+  { path: 'AddCategory', component: AddCategoryComponent }
 ];
 
 @NgModule({
--- angularproject.client/src/app/app.module.ts
+++ angularproject.client/src/app/app.module.ts
@@ -1,38 +1,40 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Components from 'adnan'
 import { NavComponent } from './adnan/nav/nav.component';
 import { FooterComponent } from './adnan/footer/footer.component';
 import { LoginComponent } from './adnan/login/login.component';
 import { RegisterComponent } from './adnan/register/register.component';
 import { ContactComponent } from './adnan/contact/contact.component';
 import { HomeComponent } from './adnan/home/home.component';
+import { AboutComponent } from './adnan/about/about.component';
+
+// Components from 'ali'
 import { CartComponent } from './ali/cart/cart.component';
 import { CheckoutComponent } from './ali/checkout/checkout.component';
 import { VoucherComponent } from './ali/voucher/voucher.component';
 
-import { AboutComponent } from './adnan/about/about.component';
-
+// Components from 'mona'
 import { FeedbackComponent } from './mona/feedback/feedback.component';
 import { UsersComponent } from './mona/users/users.component';
 import { VouchersComponent } from './mona/vouchers/vouchers.component';
-
 import { DashboardComponent } from './mona/dashboard/dashboard.component';
-import { AddCategoryComponent } from './Toqa/add-category/add-category.component';
 
-import { ShowAllCategoryComponent } from './Toqa/show-all-category/show-all-category.component';
+// Components from 'amal'
+import { ProfileandeditComponent } from './amal/profileandedit/profileandedit.component';
+import { HistoryComponent } from './amal/history/history.component';
+
+// Components from 'Toqa' (Ensure all paths are unified)
+import { AddCategoryComponent } from './Toqa/add-category/add-category.component';
 import { EditCategoryComponent } from './Toqa/edit-category/edit-category.component';
 import { AddProductComponent } from './Toqa/add-product/add-product.component';
-import { ProfileandeditComponent } from './Amal/profileandedit/profileandedit.component';
-import { HistoryComponent } from './Amal/history/history.component';
-
-
-
-
+import { ShowAllCategoryComponent } from './Toqa/show-all-category/show-all-category.component';
 
 @NgModule({
   declarations: [
@@ -45,29 +47,24 @@ import { HistoryComponent } from './Amal/history/history.component';
     HomeComponent,
     AboutComponent,
     DashboardComponent,
-    AppComponent,
     FeedbackComponent,
     UsersComponent,
     VouchersComponent,
-    DashboardComponent,
- 
-    AddCategoryComponent,
-    ShowAllCategoryComponent,
-    EditCategoryComponent,
-    AddProductComponent,
- 
     ProfileandeditComponent,
-    HistoryComponent
-    AppComponent,
+    HistoryComponent,
     CartComponent,
     CheckoutComponent,
-    VoucherComponent
+    VoucherComponent,
+    AddCategoryComponent,
+    EditCategoryComponent,
+    AddProductComponent,
+    ShowAllCategoryComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule, FormsModule
-  
-    AppRoutingModule, FormsModule 
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

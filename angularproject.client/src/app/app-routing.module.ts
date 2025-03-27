a8af55473b820dd4dc340d20e1d34c65b8ba5bac
Mona-Tamimi <munatamimi65@gmail.com>
2025-03-27 10:54:55 +0300

    mona

--- angularproject.client/src/app/app-routing.module.ts
+++ angularproject.client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './mona/users/users.component';
+import { VouchersComponent } from './mona/vouchers/vouchers.component';
+import { FeedbackComponent } from './mona/feedback/feedback.component';
+import { DashboardComponent } from './mona/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "users", component: UsersComponent },
+  { path: "vouchers", component: VouchersComponent },
+  { path: "feedback", component: FeedbackComponent },
+  { path: "dashboard", component: DashboardComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularproject.client/src/app/app.module.ts
+++ angularproject.client/src/app/app.module.ts
@@ -4,14 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './mona/feedback/feedback.component';
+import { UsersComponent } from './mona/users/users.component';
+import { VouchersComponent } from './mona/vouchers/vouchers.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './mona/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent,
+    UsersComponent,
+    VouchersComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

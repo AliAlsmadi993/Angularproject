a17dbf43308d82cf352c2896008c9114dea6f97f
Toqa Khashashneh <toqamusa@gmail.com>
2025-03-27 10:56:26 +0300

    Updates

--- angularproject.client/src/app/app-routing.module.ts
+++ angularproject.client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './Toqa/add-category/add-category.component';
+import { ShowAllCategoryComponent } from './Toqa/show-all-category/show-all-category.component';
+import { EditCategoryComponent } from './Toqa/edit-category/edit-category.component';
+import { AddProductComponent } from './Toqa/add-product/add-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ShowAllCategoryComponent },
+  { path: "AddCategory", component: AddCategoryComponent },
+  { path: "AllCategories", component: ShowAllCategoryComponent },
+  { path: "EditCategory/:id", component: EditCategoryComponent },
+  { path: "AddProduct", component: AddProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularproject.client/src/app/app.module.ts
+++ angularproject.client/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCategoryComponent } from './Toqa/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
+import { ShowAllCategoryComponent } from './Toqa/show-all-category/show-all-category.component';
+import { EditCategoryComponent } from './Toqa/edit-category/edit-category.component';
+import { AddProductComponent } from './Toqa/add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategoryComponent,
+    ShowAllCategoryComponent,
+    EditCategoryComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
